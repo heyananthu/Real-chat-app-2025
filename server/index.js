7942b3acc0f477ba9afd1080ca5d18344acdd50d
ananthu <heyananthulal@gmail.com>
2025-11-18 19:16:55 +0530

    fix: change catch-all route from * to /* for Express 5.x

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use('/api/message', messageRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/chatapp/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/chatapp/dist/index.html'));
     });
 }
